b4d163b835254f9a3ff067e73b467930c64caef7
Urik Rocksalt <610521224@gms.ndhu.edu.tw>
2023-07-25 15:18:54 +0800

    add menu model

---
+++ lib/models/menu.dart
@@ -0,0 +1,13 @@
+class Menu {
+  String id;
+  String name;
+  String description;
+  String price;
+
+  Menu(
+    this.id,
+    this.name,
+    this.description,
+    this.price,
+  );
+}
--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -1,14 +1,17 @@
+import 'package:locostall/models/menu.dart';
+
 class Shop {
   String id;
   String lang;
-  dynamic menu;
   String name;
   String shopId;
+  List<Menu> menu;
 
   Shop(
     this.id,
     this.lang,
     this.name,
     this.shopId,
+    this.menu,
   );
 }
